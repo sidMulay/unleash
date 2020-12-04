408c74475d5dc3a736645601b4b8a774f3625bdb
Siddhant Mulay <siddhant.mulay@haptik.ai>
2020-12-04 15:31:16 +0530

    test

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
-'use strict';
+// const unleash = require('unleash-server');
+const unleash = require('./lib/server-impl.js');
 
-const unleash = require('./lib/server-impl');
+// You typically will not hard-code this value in your code!
+const sharedSecret = 'dev_test_haptik123';
 
-unleash.start();
+unleash
+    .start({
+        enableLegacyRoutes: false,
+        preRouterHook: app => {
+            app.use('/api/client', (req, res, next) => {
+                if (req.header('authorization') === sharedSecret) {
+                    next();
+                } else {
+                    res.sendStatus(401);
+                }
+            });
+        },
+    })
+    .then(server => {
+        // eslint-disable-next-line no-console
+        console.log(
+            `Unleash started on http://localhost:${server.app.get('port')}`,
+        );
+    });
